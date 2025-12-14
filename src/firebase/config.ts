bc9d318c301e65c8ab88f3bc5fb8182bbe14e4f5
ayesha khan <ayeshakhan062007@gmail.com>
2025-12-14 13:46:18 +0000

    I am facing a critical Google Sign-In failure in a Firebase Web App.
    Co

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5482946143-195f5",
+  "projectId": "studio-548294613-195f5",
   "appId": "1:609245222840:web:57fcb543ef841b304da14d",
   "apiKey": "AIzaSyBmoxKYxX0hs0HSNzwC6cm5d4kuO2ofhAY",
-  "authDomain": "studio-5482946143-195f5.firebaseapp.com",
+  "authDomain": "studio-548294613-195f5.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "609245222840"
 };
